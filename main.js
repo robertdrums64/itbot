b431c85af7601909f8dc72b9b6bbb88a4f9ece1c
Robert De La Torre <robertdrums64@gmail.com>
2026-06-02 23:37:12 -0700

    TESTING CHAT API

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
-const chatBox = document.getElementById("chat-box");
+// const chatBox = document.getElementById("chat-box");
     
-function appendMessage(text, sender) {
-    const msg = document.createElement("div");
-    msg.classList.add("message", sender);
-    msg.textContent = text;
+// function appendMessage(text, sender) {
+//     const msg = document.createElement("div");
+//     msg.classList.add("message", sender);
+//     msg.textContent = text;
         
-    chatBox.appendChild(msg);
-    chatBox.scrollTop = chatBox.scrollHeight;
-}
+//     chatBox.appendChild(msg);
+//     chatBox.scrollTop = chatBox.scrollHeight;
+// }
     
-function sendMessage() {
+// function sendMessage() {
+//     const input = document.getElementById("user-input");
+//     const text = input.value.trim();
+    
+//     if (!text) return;
+        
+//     appendMessage(text, "user");
+//     input.value = "";
+        
+//     // Fake bot response (replace with API call)
+//     setTimeout(() => {
+//     appendMessage("Hi Buggy!!", "bot");
+//     }, 500);
+// }
+
+
+// REAL CHATBOT API
+
+async function sendMessage() {
     const input = document.getElementById("user-input");
-    const text = input.value.trim();
-    
+    const text = input.ariaValueMax.trim();
     if (!text) return;
-        
+
     appendMessage(text, "user");
     input.value = "";
-        
-    // Fake bot response (replace with API call)
-    setTimeout(() => {
-    appendMessage("Hi Buggy!!", "bot");
-    }, 500);
+
+    const response = await fetch("YOUR_BACKEND_ENDPOINT", {
+       method: "POST",
+       headers: { "Content-Type": "application/json"},
+       body: JSON.stringify({ message: text })
+    })
 }
